5b88960ff0b9d9a55b74ce9c740cb5f659b17ffc
Yoel Almiron <yoelalmiron@hotmail.com>
2021-10-13 12:47:51 -0300

    Agregar reto

--- src/pages/Agregar.js
+++ src/pages/Agregar.js
@@ -1,9 +1,49 @@
+import StyledNavbar from "../components/Navbar/StyledNavbar";
+import StyledButton from "../components/StyledButton";
 
 function Agregar() {
   return (
       <div className="Agregar">
-      <h6>Esta es la página de agregar</h6>
-    </div>
+        <br></br>
+      <div class="row d-flex justify-content-center mb-3">
+        <div class="col-md-6">
+            <h2 class="lead"><b>AGREGA TU RETO!</b></h2>
+            <p class="lead">
+              Deberas completar los datos para ingresar un nuevo reto.
+            </p>
+          </div>
+      </div>
+      <div class="row d-flex justify-content-center">
+        <div class="col-md-6">
+      <form>
+      
+      <div class="form-group"> 
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" id="floatingInput" placeholder="Ingrese el nombre del reto"/>
+          <label for="floatingInput">Reto</label>
+        </div>  
+      </div>
+
+      <div class="form-group">
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" id="floatingAutor" placeholder="Password"/>
+          <label for="floatingPassword">Autor</label>
+        </div>
+      </div>
+      <div class="form-group">
+      <div class="form-floating mb-3">
+        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+        <label for="floatingTextarea">Descripcion</label>
+      </div>
+      </div>
+      <div class="row d-flex justify-content-center mb-3">
+        <button type="submit" class="btn btn-primary" >Submit</button>
+      </div>
+      </form>
+      </div>
+      </div>
+
+      </div>
   );
 }
 
