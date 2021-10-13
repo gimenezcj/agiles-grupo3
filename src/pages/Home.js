1fd0930dbbc4617bee0982dca13e6c449c925bb6
gimenezcj <gimenezcj@gmail.com>
2021-10-13 15:59:58 -0300

    Solo un ejemplo para guiarme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,20 @@ import Container from "react-bootstrap/Container";
 import StyledNavbar from "../components/Navbar/StyledNavbar";
 import StyledButton from "../components/StyledButton";
 
+import Reto from "../components/Reto";
+
 function Home() {
 
   // habitos harcodeados
-  const habitos = []
+  const habitos = [
+    {text:<Reto description ="Realiza actividad fisica durante 30 minutos" bsPrefix="https://superiorcads.edu.ar/imgd/certificacion-running-maraton-fondo-medio-fondo-trail-2-019334.jpg" title="Trota"/>},
+    {text:<Reto description ="Prepara el almuerzo saludable compuesto de..." bsPrefix="https://static1.abc.es/media/bienestar/2021/04/04/batchcoocking-kEBB--620x349@abc.jpg" title="Cocina Saludable"/>}
+  ];
+
+
+
+
+
 
   return (
     <Fragment>
