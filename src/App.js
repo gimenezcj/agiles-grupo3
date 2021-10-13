bdb488b3a9e403bbc706e89068b65a49e8db7d73
ramiro-c <rami992009@gmail.com>
2021-10-13 00:31:40 -0300

    bootstrap importado

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Agregar from "./pages/Agregar";
 import Modificar from "./pages/Modificar";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <h1>Desafíame!</h1>
-        <Link to="/">Ir al home</Link>
-        <br/>
-        <Link to="/modificar">Ir al modificar</Link>
-        <br/>
-        <Link to="/agregar">Ir al agregar</Link>
-
-        <Switch>
-          <Route path="/modificar">
-            <Modificar />
-          </Route>
-          <Route path="/agregar">
-            <Agregar />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/modificar">
+          <Modificar />
+        </Route>
+        <Route path="/agregar">
+          <Agregar />
+        </Route>
+      </Switch>
     </Router>
   );
 }
