0b1b3836f14b5e126f742201d4aae091cb1d76a0
Yoel Almiron <yoelalmiron@hotmail.com>
2021-10-20 17:55:54 -0300

    Se refactorizo el codigo del agregar

--- src/pages/Agregar.js
+++ src/pages/Agregar.js
@@ -3,41 +3,43 @@ import StyledButton from "../components/StyledButton";
 
 function Agregar() {
   return (
-      <div className="Agregar">
-        <br></br>
+    <div className="Agregar">
+      <br></br>
       <div class="row d-flex justify-content-center mb-3">
         <div class="col-md-3">
-            <h2 class="lead"><b>AGREGA TU RETO!</b></h2>
-            <p class="lead">
-              Deberas completar los datos para ingresar un nuevo reto.
-            </p>
-          </div>
+          <h2 class="lead">
+            <b>AGREGA TU RETO!!</b>
+          </h2>
+          <p class="lead">
+            Deberas completar los datos para ingresar un nuevo reto.
+          </p>
+        </div>
       </div>
       <div class="row d-flex justify-content-center">
         <div class="col-md-3">
-      <form>
-      
-      <div class="form-group"> 
-        <div class="form-floating mb-3">
-          <input type="text" class="form-control" id="floatingInput" />
-          <label for="inputTitle">Titulo</label>
-        </div>  
-      </div>
+          <form>
+            <div class="form-group">
+              <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInput" />
+                <label for="inputTitle">Titulo</label>
+              </div>
+            </div>
 
-      <div class="form-group">
-      <div class="form-floating mb-3">
-        <textarea class="form-control"  id="floatingTextarea"></textarea>
-        <label for="floatingTextarea">Descripcion</label>
+            <div class="form-group">
+              <div class="form-floating mb-3">
+                <textarea class="form-control" id="floatingTextarea"></textarea>
+                <label for="floatingTextarea">Descripcion</label>
+              </div>
+            </div>
+            <div class="row d-flex justify-content-center mb-3">
+              <button type="submit" class="btn btn-primary">
+                Agregar
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-      </div>
-      <div class="row d-flex justify-content-center mb-3">
-        <button type="submit" class="btn btn-primary" >Agregar</button>
-      </div>
-      </form>
-      </div>
-      </div>
-
-      </div>
+    </div>
   );
 }
 
