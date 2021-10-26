ca01327585c94e74b4d17410ca55ad9826538c45
Francisco-G-Montero <gabixp45@gmail.com>
2021-10-26 01:50:43 -0300

    clase Reto actualizado con nuevos atributos. Metodos de RetoRepository actualizados

--- src/data/model/Reto.js
+++ src/data/model/Reto.js
@@ -1,30 +1,54 @@
 class Reto {
-  constructor(id, title, description) {
-    this.id = id;
+  constructor(
+    title,
+    description,
+    categoria,
+    fechaInic,
+    fechFin,
+    isConAmigos,
+    dailyTimestamp
+  ) {
     this.title = title;
     this.description = description;
+    this.categoria = categoria;
+    this.fechaInic = fechaInic;
+    this.fechFin = fechFin;
+    this.isConAmigos = isConAmigos;
+    this.dailyTimestamp = dailyTimestamp;
   }
   toString() {
-    return this.id + ", " + this.title + ", " + this.description;
+    return this.title + ", " + this.description;
   }
 }
 
 // Firestore data converter
 const retoConverter = {
   toFirestore: (reto) => {
-      return {
-          id: reto.id,
-          title: reto.title,
-          description: reto.description
-          };
+    return {
+      title: reto.title,
+      description: reto.description,
+      categoria: reto.categoria,
+      fechaInic: reto.fechaInic,
+      fechFin: reto.fechFin,
+      isConAmigos: reto.isConAmigos,
+      dailyTimestamp: reto.dailyTimestamp,
+    };
   },
   fromFirestore: (snapshot, options) => {
-      const data = snapshot.data(options);
-      return new Reto(data.id, data.title, data.description);
-  }
+    const data = snapshot.data(options);
+    return new Reto(
+      data.title,
+      data.description,
+      data.categoria,
+      data.fechaInic,
+      data.fechFin,
+      data.isConAmigos,
+      data.dailyTimestamp
+    );
+  },
 };
 
 module.exports = {
   retoConverter,
-  Reto
-}
+  Reto,
+};
