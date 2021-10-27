758136c828924238434028f1e28bf5f70574355a
gimenezcj <gimenezcj@gmail.com>
2021-10-27 03:33:09 -0300

    feature/vista_categorias - update: se modifico el diseño del front

--- src/components/Categoria/Categoria.js
+++ src/components/Categoria/Categoria.js
@@ -1,20 +1,31 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card,Col,Row } from "react-bootstrap";
 import Button from 'react-bootstrap/Button'
 
 function Categoria(props) {
   return (
-    <div className="container" className="mt-3" >
-      <Card border="dark" style={{ width: '17rem' , height:'31rem'}}>
-                    <Card.Img variant="top" src={props.imagen} height="250px" />
-                    <Card.Body>
-                        <Card.Title>{props.titulo}</Card.Title>
-                        <Card.Text>
-                        {props.texto}
-                        </Card.Text>
-                    <Button variant="primary">Ver Retos</Button>
-                    </Card.Body>
-                </Card>
+    <div className="container mt-{3}">
+      <Card  border="dark" style={{ width: '22rem',"borderRadius": "10px" }} rounded="true">
+        <Card.Body>
+            <Card.Img variant="top" src={props.imagen} height="250px" />
+            <Card.Text style={{"color":"gray"}}>
+              {props.fecha} - {props.estado}
+            </Card.Text>
+            <Card.Title>
+              {props.titulo}
+            </Card.Title>
+            <Card.Text style={{"color":"gray"}}>Reto. {props.detalle}</Card.Text>
+            <Row >
+              <Col md={3}></Col>
+              <Col md={6}>
+                <div className="d-grid gap-2">
+                  <Button style={{"borderRadius": "60px"}} size="sm" variant="primary" rounded="true">Ver Reto</Button>
+                </div>
+              </Col>
+              <Col md={3}></Col>
+            </Row>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
