a01e7ddb77f3fab0a3bcff4293d727c72b354cd5
ramiro-c <rami992009@gmail.com>
2021-10-29 18:17:50 -0300

    agregue las imagenes de la categoria

--- src/components/Reto/Reto.js
+++ src/components/Reto/Reto.js
@@ -2,14 +2,29 @@ import React from "react";
 import { Card, Row, Col, Image, Button } from "react-bootstrap";
 import { GrEdit } from 'react-icons/gr';
 
+import imgCorrer from "../../images/Correr.png"
+import imgCustom from "../../images/Custom.png"
+import imgFisico from "../../images/Fisico.png"
+import imgHealthy from "../../images/Healthy.png"
+import imgMental from "../../images/Mental.png"
+
 function Reto({ reto = {}, setModalShow }) {
+
+  const getSrc = ({ categoria }) => (
+    categoria == "Fisico" ? imgFisico :
+      categoria == "Correr" ? imgCorrer :
+        categoria == "Healthy" ? imgHealthy :
+          categoria == "Mental" ? imgMental : imgCustom
+  )
 
   return (
     <Col sm={4}>
-      <Card className="my-1 border-primary border-5 border-bottom-0 border-end-0 border-top-0 shadow-sm bg-body rounded">
+      <Card className="my-1 border-primary border-5 border-bottom-0 border-end-0 border-top-0 shadow bg-body rounded"
+        style={{ height: "125px" }}
+      >
         <Row>
           <Col md={3} className="align-self-center">
-            <Image src={`/images/${reto?.categoria}.jpg`} />
+            <Image width="80" height="80" src={getSrc({ categoria: reto?.categoria })} />
           </Col>
           <Col md={6}>
             <Card.Body style={{ textAlign: "left" }}>
