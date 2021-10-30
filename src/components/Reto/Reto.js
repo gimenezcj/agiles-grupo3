3caf98b96e451c42d2b7fcecd5d5d678006fbd5e
ramiro-c <rami992009@gmail.com>
2021-10-29 21:34:33 -0300

    estilos casi terminados

--- src/components/Reto/Reto.js
+++ src/components/Reto/Reto.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { Card, Row, Col, Image, Button } from "react-bootstrap";
+import { Card, Row, Col, Image, Button, Container } from "react-bootstrap";
 import { GrEdit } from 'react-icons/gr';
+
+import "./Reto.css";
 
 import imgCorrer from "../../images/Correr.png"
 import imgCustom from "../../images/Custom.png"
@@ -11,38 +13,40 @@ import imgMental from "../../images/Mental.png"
 function Reto({ reto = {}, setModalShow }) {
 
   const getSrc = ({ categoria }) => (
-    categoria == "Fisico" ? imgFisico :
-      categoria == "Correr" ? imgCorrer :
-        categoria == "Healthy" ? imgHealthy :
-          categoria == "Mental" ? imgMental : imgCustom
+    categoria === "Fisico" ? imgFisico :
+      categoria === "Correr" ? imgCorrer :
+        categoria === "Healthy" ? imgHealthy :
+          categoria ==== "Mental" ? imgMental : imgCustom
   )
+
+  const capitalize = (str) => str.toLowerCase().replace(/^\w/,
+    (c) => c.toUpperCase()
+  );
 
   return (
     <Col sm={4}>
-      <Card className="my-1 border-primary border-5 border-bottom-0 border-end-0 border-top-0 shadow bg-body rounded"
-        style={{ height: "125px" }}
-      >
-        <Row>
-          <Col md={3} className="align-self-center">
+      <Card className="my-1 border-primary border-5 border-bottom-0 \
+      border-end-0 border-top-0 shadow bg-body rounded centrar" style={{ height: "125px" }}>
+        <Container fluid className="p-0">
+          <div className="imagen">
             <Image width="80" height="80" src={getSrc({ categoria: reto?.categoria })} />
-          </Col>
-          <Col md={6}>
-            <Card.Body style={{ textAlign: "left" }}>
-              <Card.Title>{reto?.title}</Card.Title>
-              <Card.Text style={{ color: "gray" }}>{reto?.description}</Card.Text>
-            </Card.Body>
-          </Col>
-          <Col md={3} class="align-self-center">
+          </div>
+          <Card.Body className="px-0 body" style={{ textAlign: "left" }}>
+            <Card.Title>{capitalize(reto?.title)}</Card.Title>
+            <Card.Text style={{ color: "gray" }}>{capitalize(reto?.description)}</Card.Text>
+          </Card.Body>
+          <div className="footer">
             <Button
               variant="primary"
               onClick={() => setModalShow(true)}
+              className="rounded-circle"
             >
               <GrEdit />
             </Button>
-          </Col>
-        </Row>
+          </div>
+        </Container>
       </Card>
-    </ Col>
+    </Col>
   )
 }
 
