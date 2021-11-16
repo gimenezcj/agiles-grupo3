fc58d0009039dd52e02989038b7919f2842699c2
gimenezcj <gimenezcj@gmail.com>
2021-11-16 20:16:46 -0300

    Funcionalida del filtro de los retos segun la categoria seleccionada

--- src/pages/Retos.js
+++ src/pages/Retos.js
@@ -1,38 +1,40 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState ,Text} from "react";
 import Container from "react-bootstrap/Container";
 import Reto from "../components/Reto/Reto"
 import StyledNavbar from "../components/Navbar/StyledNavbar";
 import { Row } from 'react-bootstrap'
 import { getRetos } from "../data/repository/RetoRepository";
 import Button from 'react-bootstrap/Button'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import VerReto from "../components/Reto/verReto"
 
 
 function Retos(props) {
   const [habitos,setHabitos] = useState([]);
-  
+ const { search } = useLocation();
 
   useEffect(() => {
     getRetos().then(data => setHabitos(data));
   }, [])
 
 
+  let query = React.useMemo(() => new URLSearchParams(search), [search]);
+  let categoria=query.get("categoria");
 
   return (
     <Fragment>
       <StyledNavbar />
       <Container className="text-center">
-       
+
         {
           <Container className="my-5">
-               
+
               <Row style={{ justifyContent: "center" }}>
                    {
-                    habitos.map((reto, i) => <VerReto reto={reto}  key={`reto-key-${i}`}/>)    
-                  } 
+                    habitos.filter((reto)=>  reto.categoria===categoria  ).map((reto, i) => <VerReto reto={reto}  key={`reto-key-${i}`}/>)
+                  }
 
-                
+
               </Row>
             </Container>
         }
@@ -40,8 +42,8 @@ function Retos(props) {
           <Button style={{"borderRadius": "50px"}} size="xl" variant="primary" rounded="true">Volver</Button>
           </Link>
       </Container>
-      
-  
+
+
     </Fragment>
   );
 }
