86cc74a5cf7d3fac8f61febdbbf0f8615883b367
ramiro <rami992009@gmail.com>
2021-11-19 20:35:51 -0300

    conflictos resueltos

--- src/pages/Retos.js
+++ src/pages/Retos.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState ,Text} from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import StyledNavbar from "../components/Navbar/StyledNavbar";
 import { Row } from 'react-bootstrap'
@@ -7,27 +7,19 @@ import Button from 'react-bootstrap/Button'
 import { Link, useLocation } from "react-router-dom";
 import VerReto from "../components/Reto/verReto"
 
-<<<<<<< HEAD
+
 function Retos() {
   const [habitos, setHabitos] = useState([]);
-=======
-
-function Retos(props) {
-  const [habitos,setHabitos] = useState([]);
- const { search } = useLocation();
->>>>>>> 26e5aac2eff05ce212f81539fac38f6ab789f22a
+  const { search } = useLocation();
 
   useEffect(() => {
     getRetos().then(data => setHabitos(data));
   }, [])
 
-<<<<<<< HEAD
-=======
 
   let query = React.useMemo(() => new URLSearchParams(search), [search]);
-  let categoria=query.get("categoria");
+  let categoria = query.get("categoria");
 
->>>>>>> 26e5aac2eff05ce212f81539fac38f6ab789f22a
   return (
     <Fragment>
       <StyledNavbar />
@@ -35,35 +27,18 @@ function Retos(props) {
 
         {
           <Container className="my-5">
-<<<<<<< HEAD
 
             <Row style={{ justifyContent: "center" }}>
               {
-                habitos.map((reto, i) => <VerReto reto={reto} key={`reto-key-${i}`} />)
+                habitos.filter((reto) => reto.categoria === categoria && reto.isDefault === true).map((reto, i) => <VerReto reto={reto} key={`reto-key-${i}`} />)
               }
             </Row>
           </Container>
-=======
-
-              <Row style={{ justifyContent: "center" }}>
-                   {
-                    habitos.filter((reto)=>  reto.categoria===categoria && reto.isDefault===true  ).map((reto, i) => <VerReto reto={reto}  key={`reto-key-${i}`}/>)
-                  }
-
-
-              </Row>
-            </Container>
->>>>>>> 26e5aac2eff05ce212f81539fac38f6ab789f22a
         }
         <Link to="/categorias">
           <Button style={{ "borderRadius": "50px" }} size="xl" variant="primary" rounded="true">Volver</Button>
         </Link>
       </Container>
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 26e5aac2eff05ce212f81539fac38f6ab789f22a
     </Fragment>
   );
 }
