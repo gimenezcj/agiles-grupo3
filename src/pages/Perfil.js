6dbc1ac043046d7d170a7869d689e836d5193c2d
gimenezcj <gimenezcj@gmail.com>
2021-11-22 09:42:26 -0300

    Se muestran los datos del usuario {name,mail} del localStorage en pagina del Perfil

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -4,7 +4,11 @@ import { Container, Image } from "react-bootstrap";
 import StyledNavbar from "../components/Navbar/StyledNavbar";
 import StyledCalendar from "../components/StyledCalendar/StyledCalendar";
 
+//import { User } from "../data/model/User";
+
 function Perfil() {
+  const {name,mail}=JSON.parse(localStorage.getItem("user"));
+
   return (
     <Fragment>
       <StyledNavbar />
@@ -13,8 +17,8 @@ function Perfil() {
           width="150px"
           src="https://cdn3.iconfinder.com/data/icons/avatars-round-flat/33/avat-01-512.png" roundedCircle
         />
-        <h2 className="mt-2">Martin Gonzalez</h2>
-        <h6 style={{ "color": "gray" }}>marting@gmail.com</h6>
+        <h2 className="mt-2">{name}</h2>
+        <h6 style={{ "color": "gray" }}>{mail}</h6>
         <hr className="m-auto mt-4" width="200px" />
         <h2 className="mt-4">Mi calendario</h2>
         <StyledCalendar />
